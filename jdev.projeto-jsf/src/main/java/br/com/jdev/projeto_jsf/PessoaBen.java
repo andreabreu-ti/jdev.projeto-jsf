f16fce6c7c692593a7b5f98b86b77bc09c1009c6
andre abreu <aanddre@gmail.com>
2024-02-17 17:46:58 -0300

    Finalização módulo 24 JSF Introdução

--- jdev.projeto-jsf/src/main/java/br/com/jdev/projeto_jsf/PessoaBen.java
+++ jdev.projeto-jsf/src/main/java/br/com/jdev/projeto_jsf/PessoaBen.java
@@ -13,6 +13,26 @@ public class PessoaBen {
 
 	private String nome;
 
+	private String senha;
+
+	private String texto;
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
 	private HtmlCommandButton htmlCommandButton;
 
 	private List<String> nomes = new ArrayList<String>();
@@ -27,11 +47,13 @@ public class PessoaBen {
 
 	public String addNome() {
 		nomes.add(nome);
-		
+
 		if (nomes.size() > 3) {
 			htmlCommandButton.setDisabled(true);
+
+			return "paginanavegada?faces-redirect=true";
 		}
-		
+
 		return "";
 	}
 
